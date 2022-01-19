c8a46ef02bfe951a8955781d3a5adbdc2d8389c3
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2022-01-19 16:08:25 -0300

    nova feature: agora aparece quais os projetos sao novos

---
+++ scripts/projectList.js
@@ -0,0 +1,21 @@
+const projetos = [
+  {
+    nome: 'portfólio',
+    imagem: '../images/projeto-1.png',
+    descricao: 'É esta página, este card serve para exemplificar o formato dos próximos cards',
+    tags: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
+    linkProjeto: 'https://wilsonsalles-jnr.github.io/',
+    linkRepositorio: 'https://github.com/WilsonSalles-Jnr/WilsonSalles-Jnr.github.io',
+    data: '28/06/1996'
+  },
+  {
+    nome: 'sql-insert-builder',
+    imagem: '../images/projeto-2.png',
+    descricao: 'Um script para converter uma tabela para um formato insert do SQL',
+    tags: ['Javascript'],
+    linkProjeto: '',
+    linkRepositorio: 'https://github.com/WilsonSalles-Jnr/sql-insert-builder',
+    data: '28/06/1996'
+  }
+]
+export default projetos;
---
+++ scripts/projetos.js
@@ -0,0 +1,99 @@
+import projetos from "./projectList.js";
+const projectSection = document.getElementsByClassName('project-list')[0];
+
+const CARD = "card";
+const newItem = "position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger";
+const cardImg = "card-img-top";
+const cardBody = "card-body";
+const cardTitle = "card-title";
+const cardText = "card-text";
+const cardTag = "badge rounded-pill bg-warning text-dark m-1";
+const linkProject = "btn btn-primary m-1";
+const linkCode = "btn btn-secondary m-1";
+
+const checkLocal = () => {
+  if (!localStorage.visita) {
+    localStorage.visita = Date.parse('1900/01/01')
+  }
+  return localStorage.visita
+}
+
+const isNew = (data, toAppend) => {
+  const dia = data.slice(0,2);
+  const mes = data.slice(3,5);
+  const ano = data.slice(6,10);
+  if (Date.parse(`${ano}/${mes}/${dia}`) - checkLocal() > 0) {
+    const span = document.createElement('span');
+    span.className = newItem;
+    span.innerText = "NEW";
+    toAppend.appendChild(span);
+  }
+}
+
+const appendProject = (project, indice) => {
+  // CRIA O CARD
+  const section = document.createElement('section');
+  section.id = `project-${indice}`
+  section.className = CARD;
+  section.style = "width: 18rem";
+  projectSection.appendChild(section);
+  // SELECIONA O CARD RECÉM CRIADO
+  const createdCard = document.getElementById(`project-${indice}`)
+  // INSERE ITENS NO CARD
+  const image = document.createElement('img');
+  image.className = cardImg;
+  image.src = project.imagem;
+  image.alt = project.nome;
+  isNew(project.data, createdCard);
+  createdCard.appendChild(image);
+  // CRIA SEÇÃO ABAIXO DA IMAGEM
+  const body = document.createElement('section');
+  body.className = cardBody;
+  body.id = `body-${indice}`
+  createdCard.appendChild(body);
+  // SELETOR DE SEÇÃO ABAIXO DA IMAGEM
+  const createdBody = document.getElementById(`body-${indice}`);
+  // INSERE TÍTULO
+  const title = document.createElement('h3');
+  title.className = cardTitle;
+  title.innerText = project.nome;
+  createdBody.appendChild(title);
+  // INSERE DESCRIÇÃO
+  const paragraph = document.createElement('p');
+  paragraph.className = cardText;
+  paragraph.innerText = project.descricao;
+  createdBody.appendChild(paragraph);
+  // INSERE TAGS
+  project.tags.map((tag) => {
+    const span = document.createElement('span');
+    span.className = cardTag
+    span.innerText = tag;
+    createdBody.appendChild(span);
+  })
+  // INSERE QUEBRA DE LINHA
+  const breaking = document.createElement('br');
+  createdBody.appendChild(breaking);
+  // INSERE BOTÕES
+  if ( project.linkProjeto !== "") {
+    const linker = document.createElement('a');
+    linker.href = project.linkProjeto;
+    linker.target ="_blank";
+    linker.className = linkProject;
+    linker.innerText = "Ver Projeto";
+    createdBody.appendChild(linker);
+  };
+  const code = document.createElement('a');
+  code.href = project.linkRepositorio;
+  code.target = "_blank";
+  code.className = linkCode;
+  code.innerText = "Ver código";
+  createdBody.appendChild(code);
+
+}
+
+window.onload = () => {
+  console.log(projectSection)
+  projetos.map((projeto, indice) => appendProject(projeto, indice))
+  localStorage.visita = Date.now();
+
+}
